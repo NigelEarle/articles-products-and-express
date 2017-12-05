bac0905297ebb55cde7b150892c0ab89d3448b76
nwre87 <nwre87@gmail.com>
2017-12-04 14:16:58 -1000

    stub routes for articles and products

--- routes/articles.js
+++ routes/articles.js
@@ -2,14 +2,33 @@ const express = require('express');
 const Articles = require('../db/articles');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return Articles.getAllArticles()
-  .then(articles => {
-    return res.json({ articles: articles.serialize() })
+router.route('/')
+  .get('/', (req, res) => {
+    return Articles.getAllArticles()
+    .then(articles => {
+      return res.json({ articles: articles.serialize() })
+    })
+    .catch(err => {
+      return res.json(err);
+    });
   })
-  .catch(err => {
-    return res.json(err);
+  .post((req, res) => {
+    return Articles.addNewArticle()
   });
-});
 
+router.get('/new', (req, res) => {});
+
+router.route('/:title')
+  .get((req, res) => {
+
+  })
+  .put((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  });
+
+router.get('/:title/edit', (req, res) => {});
+  
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,35 @@
 const express = require('express');
+const Products = require('../db/products');
 const router = express.Router();
+
+router.route('/')
+  .get((req, res) => {
+    return Products.getAllProducts()
+    .then(products => {
+      return res.json({ products: products.serialize() })
+    })
+    .catch(err => {
+      return res.json(err);
+    })
+  })
+  .post((req, res) => {
+    return Products.addNewProduct()
+  });
+
+router.get('/new', (req, res) => {
+});
+
+router.route('/:id')
+  .get((req, res) => {
+
+  })
+  .put((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  });
+
+router.get('/:id/edit', (req, res) => {});
 
 module.exports = router;
